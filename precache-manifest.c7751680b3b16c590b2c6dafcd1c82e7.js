d966574b37f3bca067328804ce7bac7039c0aa68
iankimble <ian.kimble571@gmail.com>
2019-07-09 07:23:15 -0400

    Updates

---
+++ precache-manifest.c7751680b3b16c590b2c6dafcd1c82e7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0776e653d269f72fcfcef9f9db3bb83",
+    "url": "/nike-redesign-react-2/index.html"
+  },
+  {
+    "revision": "227064b9be6df2fbc3e2",
+    "url": "/nike-redesign-react-2/static/js/2.17e308bc.chunk.js"
+  },
+  {
+    "revision": "5ae62dea448629df059b",
+    "url": "/nike-redesign-react-2/static/js/main.11321cc1.chunk.js"
+  },
+  {
+    "revision": "c570fb5a334023641b8e",
+    "url": "/nike-redesign-react-2/static/js/runtime~main.0e566ad0.js"
+  },
+  {
+    "revision": "f399c5ed9ef3125f6f92302704c08357",
+    "url": "/nike-redesign-react-2/static/media/cart.f399c5ed.jpg"
+  },
+  {
+    "revision": "c96932fa29a84378a5beadc5228ed6fe",
+    "url": "/nike-redesign-react-2/static/media/jordan 1.c96932fa.jpg"
+  },
+  {
+    "revision": "a5d07d4f69b2cbdcd56f4a1e4dfd5c4a",
+    "url": "/nike-redesign-react-2/static/media/jordan3.a5d07d4f.jpg"
+  },
+  {
+    "revision": "1749881a076a4413b8be277cbd736b4d",
+    "url": "/nike-redesign-react-2/static/media/jordan331.1749881a.jpg"
+  },
+  {
+    "revision": "edaf9dada32eae8ef6958a79b01bc46a",
+    "url": "/nike-redesign-react-2/static/media/jordanHeroImg.edaf9dad.jpg"
+  },
+  {
+    "revision": "00d0ca25b3abf8bec4b766125ecf3103",
+    "url": "/nike-redesign-react-2/static/media/jordanSwoosh1.00d0ca25.jpg"
+  },
+  {
+    "revision": "4894945aaac13098b642ec50a43942ce",
+    "url": "/nike-redesign-react-2/static/media/jordanT1.4894945a.jpg"
+  },
+  {
+    "revision": "4c2921de4708d7bedc8d0dabccf6de8a",
+    "url": "/nike-redesign-react-2/static/media/jordanT2.4c2921de.jpg"
+  },
+  {
+    "revision": "9d937ecc2f42c1fb13100dd12afffd05",
+    "url": "/nike-redesign-react-2/static/media/kd10.9d937ecc.jpg"
+  },
+  {
+    "revision": "be26786e75c8092f4a8138536af83d93",
+    "url": "/nike-redesign-react-2/static/media/kd4.be26786e.jpg"
+  },
+  {
+    "revision": "6c9fca4c52f18a7b22cef5af4362ddeb",
+    "url": "/nike-redesign-react-2/static/media/kd7.6c9fca4c.jpg"
+  },
+  {
+    "revision": "b6bf384f4c99bfb8bbc0698918c04197",
+    "url": "/nike-redesign-react-2/static/media/kdHeroImage.b6bf384f.jpg"
+  },
+  {
+    "revision": "588d025d62e152d757d3c00ce1de6c5c",
+    "url": "/nike-redesign-react-2/static/media/kdSwoosh1.588d025d.jpg"
+  },
+  {
+    "revision": "73bf15dc1fa4534c596059d5ea4be4ed",
+    "url": "/nike-redesign-react-2/static/media/kdT1.73bf15dc.jpg"
+  },
+  {
+    "revision": "9ab597c748cc254efa3a6981ce5abbb9",
+    "url": "/nike-redesign-react-2/static/media/kdT2.9ab597c7.jpg"
+  },
+  {
+    "revision": "21ad34f5895148da95a36f34144bb33a",
+    "url": "/nike-redesign-react-2/static/media/kobe3.21ad34f5.jpg"
+  },
+  {
+    "revision": "72936dafcc4c8d08d60a3a6484b6a67d",
+    "url": "/nike-redesign-react-2/static/media/kobe7.72936daf.jpg"
+  },
+  {
+    "revision": "babf5d83fadcf64b676e7a65551e75cc",
+    "url": "/nike-redesign-react-2/static/media/kobe9.babf5d83.jpg"
+  },
+  {
+    "revision": "a4f974184581313326350da913c35657",
+    "url": "/nike-redesign-react-2/static/media/kobeHeroImg.a4f97418.jpg"
+  },
+  {
+    "revision": "e56c705a931ea507cdc2e62abbde0fb9",
+    "url": "/nike-redesign-react-2/static/media/kobeSwoosh1.e56c705a.jpg"
+  },
+  {
+    "revision": "2d45af402d8665f594d0cefaa6a4214c",
+    "url": "/nike-redesign-react-2/static/media/kobeT1.2d45af40.jpg"
+  },
+  {
+    "revision": "daef5cf4ea0cd31f6609646e11539155",
+    "url": "/nike-redesign-react-2/static/media/kobeT2.daef5cf4.jpg"
+  },
+  {
+    "revision": "59bed5eb03c78abb9975c0e01d13945d",
+    "url": "/nike-redesign-react-2/static/media/lebron10.59bed5eb.jpg"
+  },
+  {
+    "revision": "cf62bc78d2f005c8a49aeaf61dd175f1",
+    "url": "/nike-redesign-react-2/static/media/lebron11.cf62bc78.jpg"
+  },
+  {
+    "revision": "890bfa9aa4b47a95d2c926218ca4f7b1",
+    "url": "/nike-redesign-react-2/static/media/lebron13.890bfa9a.jpg"
+  },
+  {
+    "revision": "e7d2767752c49ddd2fc0902dc59fae96",
+    "url": "/nike-redesign-react-2/static/media/lebronHeroImg.e7d27677.jpg"
+  },
+  {
+    "revision": "0cb2f6ec87e9183413b45cf40b7434db",
+    "url": "/nike-redesign-react-2/static/media/lebronSwoosh1.0cb2f6ec.jpg"
+  },
+  {
+    "revision": "07a1107a1ff248981e0c9f6e8da61fb6",
+    "url": "/nike-redesign-react-2/static/media/lebronT1.07a1107a.jpeg"
+  },
+  {
+    "revision": "02d95ea8123277bb99a50ca9d8cdc354",
+    "url": "/nike-redesign-react-2/static/media/lebronT2.02d95ea8.jpg"
+  },
+  {
+    "revision": "921886e6a3f56071ccde8d3010cb77eb",
+    "url": "/nike-redesign-react-2/static/media/swoosh.921886e6.jpg"
+  }
+]);
